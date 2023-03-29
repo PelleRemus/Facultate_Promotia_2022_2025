f06f45dd040143c0fd30df676909c32e9e56e7a1
PelleRemus <remusnicolaepelle@gmail.com>
2023-03-29 22:09:34 +0300

    Pregatire pentru laborator

---
+++ Algoritmi/MatriciVizual/MatriciVizual/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace MatriciVizual
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.display = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(700, 700);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Comic Sans MS", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(718, 44);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(441, 412);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = resources.GetString("textBox1.Text");
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(718, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(288, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Please paste a matrix here:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(718, 459);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(441, 29);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Or choose one of the following algorythms:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold);
+            this.button1.Location = new System.Drawing.Point(718, 491);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 40);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Chenar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold);
+            this.button2.Location = new System.Drawing.Point(956, 491);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(200, 40);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Spirala";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // display
+            // 
+            this.display.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.display.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold);
+            this.display.Location = new System.Drawing.Point(718, 672);
+            this.display.Name = "display";
+            this.display.Size = new System.Drawing.Size(438, 40);
+            this.display.TabIndex = 6;
+            this.display.Text = "Display matrix visually";
+            this.display.UseVisualStyleBackColor = false;
+            this.display.Click += new System.EventHandler(this.display_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(718, 594);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(352, 29);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Choose the colors for the display:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1168, 720);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.display);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button display;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Algoritmi/MatriciVizual/MatriciVizual/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace MatriciVizual
+{
+    public partial class Form1 : Form
+    {
+        PictureBox[,] displays = new PictureBox[0, 0];
+        Color[] colors = new Color[]
+        {
+            Color.FromArgb(1, 33, 105),
+            Color.White,
+            Color.FromArgb(200, 16, 46),
+            Color.White,
+            Color.White,
+            Color.White,
+            Color.White,
+            Color.White
+        };
+        Point startLocation = new Point(538, 505);
+        Button[] colorButtons;
+
+        public Form1()
+        {
+            InitializeComponent();
+            colorButtons = new Button[colors.Length];
+            for(int i=0; i<colors.Length; i++)
+            {
+                Button button = new Button();
+                button.Parent = this;
+
+                button.Size = new Size(40, 40);
+                button.Location = new Point(startLocation.X + i * 41, startLocation.Y);
+                button.BackColor = colors[i];
+
+                button.Click += ColorButton_Click;
+                colorButtons[i] = button;
+            }
+        }
+
+        private void ColorButton_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorPicker = new ColorDialog();
+            if (colorPicker.ShowDialog() == DialogResult.OK)
+            {
+                int index = Array.IndexOf(colorButtons, sender as Button);
+                colors[index] = colorPicker.Color;
+                (sender as Button).BackColor = colorPicker.Color;
+            }
+        }
+
+        private void display_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < displays.GetLength(0); i++)
+                for (int j = 0; j < displays.GetLength(1); j++)
+                    displays[i, j].Parent = null;
+
+            string[][] matrix = new string[textBox1.Lines.Length][];
+            for (int i = 0; i < textBox1.Lines.Length; i++)
+                matrix[i] = textBox1.Lines[i].Split(' ');
+
+            int n = matrix.Length;
+            int m = matrix[0].Length;
+            displays = new PictureBox[n, m];
+
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < m; j++)
+                {
+                    displays[i, j] = new PictureBox();
+                    displays[i, j].Parent = pictureBox1;
+
+                    int sizeX = pictureBox1.Width / m, sizeY = pictureBox1.Height / n;
+                    displays[i, j].Size = new Size(sizeX, sizeY);
+                    displays[i, j].Location = new Point(j * sizeX, i * sizeY);
+
+                    int index = int.Parse(matrix[i][j]);
+                    displays[i, j].BackColor = colors[index];
+                }
+        }
+    }
+}
