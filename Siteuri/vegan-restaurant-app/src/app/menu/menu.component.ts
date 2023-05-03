1cb913abec21f0fc29cc5220ec7f349ed0353fa6
PelleRemus <remusnicolaepelle@gmail.com>
2023-05-03 14:00:22 +0300

    Laborator 02.05.2023 - navigare intre pagini si un card model pentru retete

--- Siteuri/vegan-restaurant-app/src/app/menu/menu.component.ts
+++ Siteuri/vegan-restaurant-app/src/app/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from '../product';
 
 @Component({
   selector: 'app-menu',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent {
-
+  product: Product = {
+    imageUrl: "https://restaumatic-production.imgix.net/uploads/accounts/34267/media_library/1e6cb911-1aa4-4a01-b9d0-07c3a8478c4c.jpg?auto=compress&blur=0&crop=focalpoint&fit=max&fp-x=0.5&fp-y=0.5&h=auto&rect=0%2C0%2C1024%2C683&w=600",
+    name: "Salată Vital",
+    description: "150 g (ţelină, sfeclă roşie, hrean, ulei presat la rece, sare, lămâie)",
+    price: 10
+  } as Product;
 }
---
+++ Siteuri/vegan-restaurant-app/src/app/product.ts
@@ -0,0 +1,6 @@
+export interface Product {
+    imageUrl: string;
+    name: string;
+    description: string;
+    price: number;
+}
