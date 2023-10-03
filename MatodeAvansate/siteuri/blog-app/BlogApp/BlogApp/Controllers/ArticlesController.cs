99e36941fa6061724573bcf92d9ba1b5262db578
PelleRemus <remusnicolaepelle@gmail.com>
2023-10-03 20:12:36 +0300

    labortator 03.10.2023 - base BE application - Articles API with CRUD operations

---
+++ MatodeAvansate/siteuri/blog-app/BlogApp/BlogApp/Controllers/ArticlesController.cs
@@ -0,0 +1,56 @@
+﻿using BlogApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApp.Controllers
+{
+    [Controller]
+    [Route("/api/[controller]")]
+    public class ArticlesController : ControllerBase
+    {
+        public static List<Article> articles = new List<Article>
+        {
+            new Article { Id = 1, AuthorId = 1, Title = "My Article", Content = "very creative content" }
+        };
+
+        [HttpGet]
+        public ActionResult GetAllArticles()
+        {
+            return new OkObjectResult(articles);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult GetArticle(int id)
+        {
+            return new OkObjectResult(articles.FirstOrDefault(article => article.Id == id));
+        }
+
+        [HttpPost]
+        public ActionResult PostArticle([FromBody] Article article)
+        {
+            articles.Add(article);
+            return new OkObjectResult(article);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult EditArticle(int id, [FromBody] Article article)
+        {
+            Article dbArticle = articles.FirstOrDefault(article => article.Id == id);
+
+            if (dbArticle != null)
+            {
+                dbArticle.Title = article.Title;
+                dbArticle.Content = article.Content;
+            }
+
+            return new NoContentResult();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteArticle(int id)
+        {
+            Article dbArticle = articles.FirstOrDefault(article => article.Id == id);
+            articles.Remove(dbArticle);
+            return new OkObjectResult(dbArticle);
+        }
+    }
+}
---
+++ MatodeAvansate/siteuri/blog-app/BlogApp/BlogApp/Models/Article.cs
@@ -0,0 +1,10 @@
+﻿namespace BlogApp.Models
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public int AuthorId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
