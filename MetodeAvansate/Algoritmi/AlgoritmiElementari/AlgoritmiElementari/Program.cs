4961416cfd04f890e2cc5b6070123420ddc7253a
PelleRemus <remusnicolaepelle@gmail.com>
2023-11-16 19:38:57 +0200

    laborator 16.11.2023 - grafuri elementare si divide  and conquer

---
+++ MetodeAvansate/Algoritmi/AlgoritmiElementari/AlgoritmiElementari/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgoritmiElementari
+{
+    internal class Program
+    {
+        static int n;
+        static int[,] matrix;
+        static bool[] visited;
+
+        static void Main(string[] args)
+        {
+            n = 7;
+            matrix = new int[,]
+            {
+                { 0, 1, 0, 0, 1, 0, 0 },
+                { 1, 0, 1, 0, 0, 0, 0 },
+                { 0, 1, 0, 1, 0, 0, 0 },
+                { 0, 0, 1, 0, 0, 0, 0 },
+                { 1, 0, 0, 0, 0, 1, 0 },
+                { 0, 0, 0, 0, 1, 0, 1 },
+                { 0, 0, 0, 0, 0, 1, 0 },
+            };
+            visited = new bool[n];
+
+            //Console.WriteLine(IsHamiltonian());
+            Console.ReadKey();
+        }
+
+        static bool IsHamiltonian()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                visited = new bool[n];
+                List<int> noduri = new List<int>();
+                noduri.Add(i);
+                GasireDrum(noduri);
+                if (noduri.Count == n)
+                    return true;
+            }
+            return false;
+        }
+
+        static void GasireDrum(List<int> noduri)
+        {
+            int nod = noduri[noduri.Count - 1];
+            visited[nod] = true;
+
+            for (int j = 0; j < n; j++)
+                if (matrix[nod, j] == 1 && !visited[j])
+                {
+                    visited[j] = true;
+                    noduri.Add(j);
+                    GasireDrum(noduri);
+                    return;
+                }
+        }
+    }
+}
---
+++ MetodeAvansate/Algoritmi/DivideAndConquer/DivideAndConquer/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivideAndConquer
+{
+    internal class Program
+    {
+        static int n;
+        static int[] array;
+
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(Recursiv('d'));
+            n = 10;
+            array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Console.WriteLine(BinarySearch(7, 0, n - 1));
+            Console.ReadKey();
+        }
+
+        // a
+        // aba
+        // abacaba
+        // abacabadabacaba
+        static string Recursiv(char c)
+        {
+            if (c == 'a')
+                return "a";
+            return Recursiv((char)(c - 1)) + c + Recursiv((char)(c - 1));
+        }
+
+        // Binary Search functioneaza doar pentru array-uri deja sortate
+        static bool BinarySearch(int toFind, int left, int right)
+        {
+            if (left >= right)
+                return false;
+            int middle = (left + right) / 2;
+
+            if (toFind < array[middle])
+                return BinarySearch(toFind, left, middle);
+            if (toFind == array[middle])
+                return true;
+            //if (toFind > array[middle])
+            return BinarySearch(toFind, middle, right);
+        }
+    }
+}
