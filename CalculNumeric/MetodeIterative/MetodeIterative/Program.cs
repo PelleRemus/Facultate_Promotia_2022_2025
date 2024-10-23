5b81ff649ce02a2268ec50c52a4146723307e55f
PelleRemus <remusnicolaepelle@gmail.com>
2024-10-23 19:03:59 +0300

    program.cs

--- CalculNumeric/MetodeIterative/MetodeIterative/Program.cs
+++ CalculNumeric/MetodeIterative/MetodeIterative/Program.cs
@@ -4,10 +4,34 @@ namespace MetodeIterative
 {
     public class Program
     {
-        static decimal a, b, epsilon;
+        static int n;
+        static decimal a, b, dda, xn, xn_1, epsilon;
 
         static void Main(string[] args)
         {
+            a = 0; b = 1; dda = 0; epsilon = 0.0001m; // 1e-4m;
+            decimal aux;
+            n = 0;
+
+            // Valoarea lui x0 este in functie de f(a)*f"(a)
+            if (f(a) * dda < 0)
+            {
+                xn_1 = a;
+                aux = b;
+            }
+            else
+            {
+                xn_1 = b;
+                aux = a;
+            }
+
+            // Metoda coardei in sine:
+
+        }
+
+        static decimal f(decimal x)
+        {
+            return x * x * x - 3 * x + 1;
         }
     }
 }
