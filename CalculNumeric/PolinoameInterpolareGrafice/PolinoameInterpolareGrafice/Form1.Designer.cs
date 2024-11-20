5fa00acbd8afd1a57ce9911442aa26bed04e2744
PelleRemus <remusnicolaepelle@gmail.com>
2024-11-20 20:08:19 +0200

    Newton interpolare, desenare grafic

---
+++ CalculNumeric/PolinoameInterpolareGrafice/PolinoameInterpolareGrafice/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace PolinoameInterpolareGrafice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1275, 765);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.RoyalBlue;
+            this.button1.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(1293, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 48);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Newton";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1475, 789);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ CalculNumeric/PolinoameInterpolareGrafice/PolinoameInterpolareGrafice/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PolinoameInterpolareGrafice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Graphics graphics;
+        Bitmap bitmap;
+        decimal x0, xn, y0, yn;
+
+        int m;
+        decimal[] x, y, u;
+        decimal[,] Nf;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            m = 5;
+            x = new decimal[] { 1, 1.1m, 1.3m, 1.5m, 1.6m };
+            y = new decimal[] { 1, 1.032m, 1.091m, 1.145m, 1.17m };
+            x0 = x[0];
+            xn = x[m - 1];
+            y0 = y[0];
+            yn = y[m - 1];
+
+            // Pasul 1, calculam diferentele divizate
+            decimal[,] d = new decimal[m, m];
+            for (int i = 0; i < m; i++)
+                d[0, i] = y[i];
+
+            for (int j = 1; j < m; j++)
+                for (int i = 0; i < m - j; i++)
+                    d[j, i] = (d[j - 1, i + 1] - d[j - 1, i]) / (x[i + 1] - x[i]);
+
+            // Pasul 2, calculam Nf
+            decimal h = (xn - x0) / 1000;
+            u = new decimal[1000];
+            Nf = new decimal[m, 1000];
+            for (int j = 0; j < 1000; j++)
+            {
+                u[j] = x[0] + j * h;
+                Nf[0, j] = y[0];
+                decimal[] p = new decimal[m];
+                p[0] = 1;
+                for (int i = 1; i < m; i++)
+                {
+                    p[i] = p[i - 1] * (u[j] - x[i - 1]);
+                    Nf[i, j] = Nf[i - 1, j] + p[i] * d[i, 0];
+                }
+            }
+
+            decimal[] f = new decimal[1000];
+            for (int i = 0; i < 1000; i++)
+                f[i] = Nf[m - 1, i];
+
+            // Pasul 3, desenare grafic
+            DrawGraph(u, f);
+        }
+
+        public void DrawGraph(decimal[] x, decimal[] y)
+        {
+            bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            graphics = Graphics.FromImage(bitmap);
+
+            for (int i = 0; i < x.Length; i++)
+            {
+                PointF location = MapValuesToPointF(x[i], y[i]);
+                graphics.DrawEllipse(new Pen(Color.Red, 9),
+                    location.X - 5, location.Y - 5, 11, 11);
+            }
+
+            pictureBox1.Image = bitmap;
+        }
+
+        public PointF MapValuesToPointF(decimal x, decimal y)
+        {
+            decimal scaleX = xn - x0;
+            decimal scaleY = yn - y0;
+            float X = (float)((x - x0) * (pictureBox1.Width - 5) / scaleX);
+            float Y = pictureBox1.Height - (float)((y - y0) * (pictureBox1.Height - 5) / scaleY);
+            return new PointF(X, Y);
+        }
+    }
+}
