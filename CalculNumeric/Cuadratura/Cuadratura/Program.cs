b9133ee009e077997eac16a3ae90efd62df7ea22
PelleRemus <remusnicolaepelle@gmail.com>
2024-12-18 18:40:53 +0200

    program de baza pentru cuadratura

---
+++ CalculNumeric/Cuadratura/Cuadratura/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cuadratura
+{
+    internal class Program
+    {
+        static decimal a, b;
+        static int n;
+
+        static void Main(string[] args)
+        {
+            a = 0; b = 1;
+            n = 100;
+
+            decimal[] x = new decimal[n + 1];
+            // ...
+        }
+
+        static decimal f(decimal x)
+        {
+            return 1M / (x + 1);
+        }
+    }
+}
